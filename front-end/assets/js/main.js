f6c8c4af12c27c58c712a1c31369a187a8b2d9bd
Carlos Henrique <carloss.chac@gmail.com>
2022-11-10 14:01:55 -0300

    Commit
    Atualização no front end

--- front-end/assets/js/main.js
+++ front-end/assets/js/main.js
@@ -1,10 +1,29 @@
 /* global bootstrap: false */
-;(() => {
-  'use strict'
+(() => {
+  "use strict";
   const tooltipTriggerList = Array.from(
     document.querySelectorAll('[data-bs-toggle="tooltip"]')
-  )
-  tooltipTriggerList.forEach(tooltipTriggerEl => {
-    new bootstrap.Tooltip(tooltipTriggerEl)
-  })
-})()
+  );
+  tooltipTriggerList.forEach((tooltipTriggerEl) => {
+    new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+})();
+
+$("#cadastroFuncionario").click(function () {
+  let formCadastro = $(".view-content");
+  formCadastro.html(`<div class="view-content-teste">
+  <h1>Cadastrar Funcionario</h1>
+    <form action="Inserir API AQUI" method="post">
+      <div class="row formularios align-items-center">
+        <input type="text" placeholder="Insira o nome aqui" required>
+        <input type="number" placeholder="Insira o cpf aqui" required>
+        <select name="selectSexo">
+          <option selected>Selecione um gênero</option>
+          <option value="m">Masculino</option>
+          <option value="f">Feminino</option>
+        </select>
+      </div>
+    </form>
+</div>
+  `);
+});
