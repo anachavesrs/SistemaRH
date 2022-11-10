c8a4db2c3a9b4968d236cdb009656061725cc2c8
Carlos Henrique <92608686+carloschac@users.noreply.github.com>
2022-11-10 19:48:19 -0300

    commit

--- front-end/assets/js/main.js
+++ front-end/assets/js/main.js
@@ -8,11 +8,33 @@
     new bootstrap.Tooltip(tooltipTriggerEl);
   });
 })();
-
+//Criando formulário de cadastro funcionario
 $("#cadastroFuncionario").click(function () {
-  let formCadastro = $(".view-content");
-  formCadastro.html(`<div class="view-content-teste">
-  <h1>Cadastrar Funcionario</h1>
+$(".view-content").html(`<div class="view-content-teste">
+<h1>Cadastrar Funcionario</h1>
+  <form action="Inserir API AQUI" method="post">
+    <div class="row formularios align-items-center">
+      <input type="text" placeholder="Insira o nome aqui" required>
+      <input type="number" placeholder="Insira o cpf aqui" required>
+      <select name="selectSexo">
+        <option selected>Selecione um gênero</option>
+        <option value="m">Masculino</option>
+        <option value="f">Feminino</option>
+      </select>
+    </div>
+      <div class="buttonEnviar pt-3">
+        <button type="button" class="btn btn-primary" id="submitForm">Enviar
+        </button>
+      </div>
+  </form>
+</div>
+`);
+});
+
+//Criando formulário de cadastro de setor
+$("#cadastroSetor").click(function () {
+  $(".view-content").html(`<div class="view-content-teste">
+  <h1>Cadastrar Setor</h1>
     <form action="Inserir API AQUI" method="post">
       <div class="row formularios align-items-center">
         <input type="text" placeholder="Insira o nome aqui" required>
@@ -24,6 +46,32 @@ $("#cadastroFuncionario").click(function () {
         </select>
       </div>
     </form>
-</div>
+  </div>
   `);
-});
+  });
+  
+
+//Criando formulário de cadastro folha
+$("#cadastroFolha").click(function () {
+  $(".view-content").html(`<div class="view-content-teste">
+  <h1>Cadastrar Folha</h1>
+    <form action="Inserir API AQUI" method="post">
+      <div class="row formularios align-items-center">
+        <input type="text" placeholder="Insira o nome aqui" required>
+        <input type="number" placeholder="Insira o cpf aqui" required>
+        <select name="selectSexo">
+          <option selected>Selecione um gênero</option>
+          <option value="m">Masculino</option>
+          <option value="f">Feminino</option>
+        </select>
+      </div>
+    </form>
+  </div>
+  `);
+  });
+
+  //Função para exibir mensagem ao clicar no botão de enviar
+  $('#submitForm').click(function(){
+    alert('Enviado com sucesso!')
+  })
+  
